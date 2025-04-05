31f45189cd90e22e42f8bdb65935e233f5009884
Kerim <maxirwellisdead@gmail.com>
2025-04-05 23:56:42 +1000

    translation fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './i18n';
 import './assets/styles/global.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
